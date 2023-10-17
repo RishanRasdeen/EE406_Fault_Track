a71ef7d96300fbcfcacd6c9135bd3a4bccc1d875
Rishan98rasdeen <rishan25shehan@gmail.com>
2023-10-17 20:13:34 +0530

    Add files via upload

---
+++ textGen/include/numbering.hpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <regex>
+
+using namespace std;
+
+void numberNodes(const string &inputFileName,
+                 map<string, int> &inputNodes,
+                 map<string, int> &outputNodes,
+                 map<string, int> &wireNodes);
